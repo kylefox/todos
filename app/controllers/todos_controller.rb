f90cf5a01703eb174fe8963a559018fe32c6d0ca
Kyle Fox <kylefox@gmail.com>
2025-06-18 07:34:27 -0600

    Update todos_controller.rb

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -29,13 +29,7 @@ class TodosController < ApplicationController
       end
     else
       respond_to do |format|
-        format.turbo_stream {
-          render turbo_stream: turbo_stream.replace(
-            @todo,
-            partial: "todos/todo",
-            locals: { todo: @todo }
-          )
-        }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(@todo) }
         format.html { redirect_to todos_path, alert: @todo.errors.full_messages.join(", ") }
       end
     end
